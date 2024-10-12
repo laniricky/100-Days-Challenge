89e0d4d2f8cfba9e29268702a3a4c037a33a246b
lani <fredricklani@gmail.com>
2024-10-12 17:00:38 +0300

    DAY 3

---
+++ projects/day_3/script.js
@@ -0,0 +1,7 @@
+const quotes = [
+	"Do or do not. There is no try. - Yoda",
+	"The best way to predict the future is to invent it. - Alan Kay",
+	"Code is like humor. When you have to explain it, it’s bad. - Cory House",
+	"In order to be irreplaceable, one must always be different. - Coco Chanel",
+	"The only way to do great work is to love what you do. - Steve Jobs"
+];
