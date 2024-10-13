dc92348e802897d06587f2a8e869d36cae22e1af
LANI <fredricklani@gmail.com>
2024-10-13 10:55:58 +0300

    DAY 5

---
+++ projects/day_5/basic_calculator.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char operation;
+    float num1, num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch (operation) {
+        case '+':
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << "Result: " << num1 / num2 << endl;
+            else
+                cout << "Error: Division by zero!" << endl;
+            break;
+        default:
+            cout << "Invalid operator!" << endl;
+    }
+
+    return 0;
+}
+
