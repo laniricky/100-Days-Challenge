c44f96e592b4bc21f657419cfc30ea867f753097
LANI <fredricklani@gmail.com>
2024-10-13 11:01:12 +0300

    DAY 5

---
+++ projects/day_5/personal_info_form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Personal Info Form</title>
+</head>
+<body>
+    <h1>Personal Information Form 📋</h1>
+    <form method="POST" action="display_info.php">
+        Name: <input type="text" name="name" required><br><br>
+        Age: <input type="number" name="age" required><br><br>
+        Email: <input type="email" name="email" required><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
