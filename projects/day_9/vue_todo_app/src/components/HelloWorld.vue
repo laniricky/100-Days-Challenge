81faab639b7ed27502d5b49ef5ee7beae3c30a8a
LANI <fredricklani@gmail.com>
2024-10-17 09:00:34 +0300

    DAY 9

---
+++ projects/day_9/vue_todo_app/src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="todo">
+    <h1>Vue.js To-Do List ✅</h1>
+    <input v-model="newTask" @keyup.enter="addTask" placeholder="Add a new task" />
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task }} <button @click="removeTask(index)">❌</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: []
+    }
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== '') {
+        this.tasks.push(this.newTask)
+        this.newTask = ''
+      }
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+.todo {
+  text-align: center;
+  margin-top: 50px;
+}
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  margin: 5px 0;
+}
+button {
+  margin-left: 10px;
+  background-color: red;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+</style>
+
