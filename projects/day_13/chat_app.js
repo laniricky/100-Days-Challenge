c464531e90e0dcdc416e0df9e47b6704d3128add
LANI <fredricklani@gmail.com>
2024-10-22 09:51:12 +0300

    DAY 13

---
+++ projects/day_13/chat_app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Listening on *:3000');
+});
+
