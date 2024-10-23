8e96210d5afb39a85e19a485b8cdb19b9fa079b7
LANI <fredricklani@gmail.com>
2024-10-23 12:52:00 +0300

    DAY 15

---
+++ projects/day_15/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "encoding/json"
+    "net/http"
+)
+
+type Task struct {
+    ID   int    `json:"id"`
+    Name string `json:"name"`
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+    tasks := []Task{
+        {ID: 1, Name: "Learn GoLang"},
+        {ID: 2, Name: "Build a REST API"},
+	{ID: 3, Name: "Follow My GitHub Profile"},
+    }
+    json.NewEncoder(w).Encode(tasks)
+}
+
+func main() {
+    http.HandleFunc("/tasks", getTasks)
+    http.ListenAndServe(":8080", nil)
+}
+
